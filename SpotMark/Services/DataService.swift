fbbcf57befac08b7584acc01d56bcf6e8b63eaa6
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-20 20:03:17 +0800

    Initiated DataService

--- SpotMark/Services/DataService.swift
+++ SpotMark/Services/DataService.swift
@@ -8,10 +8,34 @@
 
 import Foundation
 import Firebase
+import Firebase
+
+let DB_BASE = Database.database().reference()
 
 class DataService {
     
     static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_PLACES = DB_BASE.child("places")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_PLACES: DatabaseReference {
+        return _REF_PLACES
+    }
+    
+    func createDbUser(uid: String, userData: Dictionary<String, Any>){
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
     
     
 }
