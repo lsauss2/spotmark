32208a95d38c85d428a33d90442d30d64b5f8979
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-21 18:00:01 +0800

    Created outlets for empty state

--- SpotMark/Controller/SpotsListVC.swift
+++ SpotMark/Controller/SpotsListVC.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class SpotsListVC: UIViewController {
+    
+    @IBOutlet weak var emptyImage: UIImageView!
+    @IBOutlet weak var emptyLabel: UILabel!
+    @IBOutlet weak var emptyButton: CustomButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +29,6 @@ class SpotsListVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
