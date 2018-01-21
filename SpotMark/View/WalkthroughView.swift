90b5f3cd9277efdf1d9bd92e5b032e493de1aca0
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-21 13:42:38 +0800

    Walkthrough view created

---
+++ SpotMark/View/WalkthroughView.swift
@@ -0,0 +1,18 @@
+//
+//  WalkthroughView.swift
+//  SpotMark
+//
+//  Created by Ludovic Saussinan on 21/1/18.
+//  Copyright © 2018 Ludovic Saussinan. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughView: UIView {
+    
+    @IBOutlet weak var walktrhroughImage: UIImageView!
+    @IBOutlet weak var walkthroughHeader: UILabel!
+    @IBOutlet weak var walkthroughDetails: UILabel!
+    
+
+}
