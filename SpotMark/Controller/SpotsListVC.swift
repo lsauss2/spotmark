66a65f758f833095029b5cf48c2c44a7deb6c6c2
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-21 20:42:15 +0800

    Added segue from empty

--- SpotMark/Controller/SpotsListVC.swift
+++ SpotMark/Controller/SpotsListVC.swift
@@ -24,6 +24,9 @@ class SpotsListVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func addPlaceFromEmpty (_ sender: Any) {
+        performSegue(withIdentifier: "showAddPlaceFromEmpty", sender: nil)
+    }
     
 
 
