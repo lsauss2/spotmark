78dafb36b6d32f72293aa837232d138d9f0ee51a
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-21 14:03:14 +0800

    Carrousel setup done

--- SpotMark/Controller/WalkthroughVC.swift
+++ SpotMark/Controller/WalkthroughVC.swift
@@ -8,28 +8,55 @@
 
 import UIKit
 
-class WalkthroughVC: UIViewController {
-
+class WalkthroughVC: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let feature1 = ["title": "Header number one", "description": "Description number one", "image": "walkthrough-one"]
+    let feature2 = ["title": "Header number two", "description": "Description number two", "image": "walkthrough-two"]
+    let feature3 = ["title": "Header number three", "description": "Description number three", "image": "walkthrough-three"]
+    
+    var featureArray = [Dictionary<String, String>]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        scrollView.delegate = self
+        
+        featureArray = [feature1, feature2, feature3]
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: self.view.bounds.width * 3, height: 500)
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        loadFeatures()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func loadFeatures() {
+        
+        for (index, feature) in featureArray.enumerated() {
+            if let featureView = Bundle.main.loadNibNamed("WalkthroughView", owner: self, options: nil)?.first as? WalkthroughView {
+                
+                featureView.walktrhroughImage.image = UIImage(named: feature["image"]!)
+                featureView.walkthroughHeader.text = feature["title"]
+                featureView.walkthroughDetails.text = feature["description"]
+                
+                scrollView.addSubview(featureView)
+                featureView.frame.size.width = self.view.bounds.size.width
+                featureView.frame.origin.x = CGFloat(index) * self.view.bounds.size.width
+                
+            }
+            
+        }
+        
     }
-    */
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let page = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(page)
+        
+    }
 
 }
