bde2e68304ab9ea8504fadd0f73116349de1e335
Ludovic Saussinan <ludo@Ludovics-MacBook-Pro.local>
2018-01-21 19:17:42 +0800

    Basic setup

--- SpotMark/Controller/AddPlaceVC.swift
+++ SpotMark/Controller/AddPlaceVC.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class AddPlaceVC: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -21,15 +26,24 @@ class AddPlaceVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeBtnTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
+}
+
+extension AddPlaceVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
